575a285590c9c1d08b396c19bc9b4a0e19a13dfe
dgerontop <dgerontop@OLDBIRD-PC>
2016-09-29 01:13:57 +0300

    Radar Tui Version 1.02 ++Add 	#UDP Client 	#UDP Server 	#give args to radar if mode is server or client 		- Repair args given by user

--- src/radar/Radar.java
+++ src/radar/Radar.java
@@ -15,22 +15,21 @@ public class Radar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        
         String mode;
-        if (args[1] != null) {
-            if (args[1].equals("server")) {
-                 mode = args[1];
-            }else {
-                 mode = "client";
-            }
-        }else {
-            mode = "standalone"; 
+        
+        if (args[0].equals("server")) {
+            mode = args[0];
+        } else if (args[0].equals("client")) {
+            mode = args[0];
+        } else {
+            mode = "standalone";            
         }
         
         RadarHandler test = new RadarHandler();
-       Tui test2 = new Tui();
-       test2.tuishellStart(mode);
-       
+        Tui test2 = new Tui();
+        test2.tuishellStart(mode);
+        
     }
     
 }
